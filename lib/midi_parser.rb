e32d61533a8697f79cb5ae15e39c9ae816e76114
André Fonseca <andre.amorimfonseca@gmail.com>
2012-06-18 23:29:45 +0200

    - Parsing midi with midilib - first try.

--- lib/midi_parser.rb
+++ lib/midi_parser.rb
@@ -1,7 +1,55 @@
-require 'unimidi'
+require 'midilib/sequence'
+require 'midilib/io/seqreader'
+
+class FileNotFoundError < StandardError ; end
 
 class MidiParser
   
+  public
+  
+  def initialize(file_or_folder_path)
+    read_files(file_or_folder_path)
+  end
+  
+  def add_resource(file_or_folder_path)
+    read_files(file_or_folder_path)
+  end
+  
+  private
+  
+  def read_files(file_or_folder_path)  
+    raise FileNotFoundError unless File.exists?(file_or_folder_path)
+    
+    files = []
+    if File.directory?(file_or_folder_path) then
+      files += Dir.glob("#{file_or_folder_path}/**/*.mid")
+    else
+      files << file_or_folder_path if File.extname(file_or_folder_path) == ".mid"
+    end
+    
+    if files.empty? then
+      puts "No files were added."
+      return
+    end 
+    
+    files.each { |file_path|
+      read_midi(file_path)
+    }
+  end
+  
+  def read_midi(file_path)
+    # Create a new, empty sequence.
+    seq = MIDI::Sequence.new()
+    
+    # Read the contents of a MIDI file into the sequence.
+    File.open(file_path, 'rb') { | file |
+        seq.read(file) { | track, num_tracks, i |
+            # Print something when each track is read.
+            puts "read track #{i} of #{num_tracks} => #{track.inspect}"
+        }
+    }
+  end
+    
 end
 
 class MidiElement
