e00952539c796f9757a62bf871eb0d31720d4142
André Fonseca <andre.amorimfonseca@gmail.com>
2012-07-29 22:13:11 +0200

    - Adding comments

--- lib/musikov/markov_model.rb
+++ lib/musikov/markov_model.rb
@@ -1,18 +1,30 @@
 
 module Musikov
-  
+
+# This class represents a generic markov chain. It holds
+# a hash of frequencies of subsequent states, for each state.
 class MarkovModel
   
   attr_reader :frequencies
   
+  ###################
   public
+  ###################
   
+  # Initialize the hashes used to build the markov chain.
+  # Passes the initial array of values to be included in the model.
+  # * The "transitions" hash maps a state into another hash mapping the subsequent states to its number of subsequent occurrences
+  # * The "frequencies" hash maps a state into another hash mapping the subsequent states to a frequency indicating the probability of subsequent occurrences.
   def initialize(value_chain = [])
     @transitions = {}
     @frequencies = {}
     add_input(value_chain)
   end
   
+  # Generate a random sequence from a markov chain
+  # * The initial_value is a state where the random chain must start
+  # * The initial_value may be nil
+  # * The value_number indicates the number of elements on the result random sequence
   def generate(initial_value, value_number)
     generated_sequence << initial_value
     selected_value = initial_value
@@ -26,6 +38,7 @@ class MarkovModel
     return generated_sequence
   end
   
+  # Passes the argument array of state values to be included in the model.
   def add_input(value_chain = [])
     prev_value = nil
     
@@ -37,13 +50,17 @@ class MarkovModel
     compute_frequencies
   end
   
+  ###################
   private
+  ###################
   
+  # Add a state on the transitions hash
   def add_value(prev_value = nil, value) 
     @transitions[prev_value] ||= Hash.new{0}
     @transitions[prev_value][value] += 1
   end
   
+  # Pick a value on the frequencies hash based on a random number and the previous state
   def pick_value(random_number, prev_value)
     succ_list = @frequencies[prev_value].sort
     freq_counter = 0.0
@@ -54,6 +71,7 @@ class MarkovModel
     }
   end
   
+  # Compute the frequencies hash based on the transistions hash
   def compute_frequencies
     @transitions.map { |value, transition_hash|
       sum = 0.0
